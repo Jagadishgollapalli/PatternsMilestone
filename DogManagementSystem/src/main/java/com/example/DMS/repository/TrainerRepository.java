801dee9c26394145ac048dead917ef9871a1ac6b
Jagadish G <jagadishgollapalli24@gmail.com>
2023-11-14 20:25:50 -0600

    Initial commit

---
+++ DogManagementSystem/src/main/java/com/example/DMS/repository/TrainerRepository.java
@@ -0,0 +1,12 @@
+/**
+ * Jagadish gollapalli
+ */
+package com.example.DMS.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.DMS.Model.Trainer;
+
+public interface TrainerRepository extends CrudRepository<Trainer, Integer> {
+
+}
